e9c1013d90e567cd99563b53a94a5983484dbd9a
sofia tofigh <sst2140@columbia.edu>
2020-03-06 05:00:21 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,12 @@
 
 def using_while
+  
   levitation_force = 6
-  levitation_force= levitation_force+1
+  
   while levitation_force <10
 	puts "Wingardium Leviosa"
+	
+	levitation_force= levitation_force+1
 	
 end
 
